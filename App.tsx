d0ec2d33ccbaf55964b4cc99b1f9feb6c436b9cd
NacreousDawn596 <kamalafriadwac@gmail.com>
2024-03-23 23:32:50 +0000

    some enhancements

--- App.tsx
+++ App.tsx
@@ -1,21 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Animated, Easing } from 'react-native';
+import { Text, View, StyleSheet, Animated, Easing, StatusBar, PanResponder, Dimensions } from 'react-native';
 
 const App = () => {
+    StatusBar.setHidden(true, 'none');
     const [time, setTime] = useState(new Date());
-    const [nextTime, setNextTime] = useState(new Date(time.getTime() + 1000));
     const [hourAnim] = useState(new Animated.Value(0));
     const [minuteAnim] = useState(new Animated.Value(0));
     const [secondAnim] = useState(new Animated.Value(0));
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setTime(new Date());
-            setNextTime(new Date(time.getTime() + 1000));
-        }, 1000);
-
-        return () => clearInterval(interval);
-    }, [time]);
+    const [colorIndex, setColorIndex] = useState(0);
+    const colors = [
+        ["rgb(240, 248, 255)", "rgb(0, 0, 0)", 'rgba(0, 0, 0, 0.6)'],
+        ["rgb(240, 248, 255)", "rgb(255, 192, 203)", 'rgba(255, 192, 203, 0.6)'],
+        ['rgb(240, 248, 255)', 'rgb(255, 0, 0)', 'rgba(255, 0, 0, 0.6)'],
+        ['rgb(240, 248, 255)', 'rgb(0, 0, 255)', 'rgba(0, 0, 255, 0.6)'],
+        ['rgb(240, 248, 255)', 'rgb(128, 0, 128)', 'rgba(128, 0, 128, 0.6)'],
+        ["rgb(0, 0, 0)", "rgb(240, 248, 255)", 'rgba(240, 248, 255, 0.6)'],
+    ]
 
     useEffect(() => {
         Animated.timing(hourAnim, {
@@ -48,8 +48,35 @@ const App = () => {
         return hours >= 12 ? 'PM' : 'AM';
     };
 
+    const screenHeight = Dimensions.get('window').height;
+    const swipeThreshold = 4 * screenHeight / 9;
+
+    const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onPanResponderRelease: (evt, gestureState) => {
+            if (gestureState.dy < -swipeThreshold) {
+                setColorIndex((prevIndex) => (prevIndex + 1) % colors.length);
+                styles = StyleSheet.create({
+                    ...styles,
+                    container: {
+                        ...styles.container,
+                        backgroundColor: colors[colorIndex][0]
+                    },
+                    textb: {
+                        ...styles.textb,
+                        textShadowColor: colors[colorIndex][2],
+                    },
+                    textt: {
+                        ...styles.textt,
+                        color: colors[colorIndex][1],
+                    }
+                });
+            }
+        }
+    });
+
     return (
-        <View style={styles.container}>
+        <View style={styles.container} {...panResponder.panHandlers}>
             <View style={[styles.timeContainer, { zIndex: 2 }]}>
                 <View style={styles.time}><Text style={[styles.textt, styles.texts]}>{getPeriod(time.getHours())}</Text></View>
                 <Animated.Text style={[styles.textt, styles.textb, { transform: [{ scale: hourAnim.interpolate({ inputRange: [0, 12], outputRange: [1, 1.2] }) }] }]}>{formatTime(time.getHours() % 12)}</Animated.Text>
@@ -60,10 +87,10 @@ const App = () => {
     );
 };
 
-const styles = StyleSheet.create({
+var styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#000",
+        backgroundColor: "rgb(0, 0, 0)",
         alignItems: "center",
         justifyContent: "center",
     },
@@ -74,24 +101,30 @@ const styles = StyleSheet.create({
     },
     time: {
         position: "absolute",
-        width: "140%",
+        width: "200%",
         height: "100%",
         marginLeft: "50%",
         marginTop: "5%"
     },
     textt: {
-        color: "#f0f8ffec",
-        textShadowColor: 'rgba(255, 255, 255, 0.6)', 
-        textShadowOffset: { width: 0, height: 0 }, 
-        textShadowRadius: 20, 
+        color: "rgb(240, 248, 255)",
     },
     textb: {
         fontSize: 250,
+        textShadowColor: 'rgba(240, 248, 255, 0.6)',
+        textShadowOffset: { width: 0, height: 0 },
+        textShadowRadius: 20,
+        borderRadius: 20,
+        overflow: "hidden",
     },
     texts: {
-        fontSize: 25,
-        fontWeight: "bold"
-    }
+        fontSize: 20,
+        fontWeight: "bold",
+    },
+    image: {
+        width: 100,
+        height: 100,
+    },
 });
 
 export default App;
