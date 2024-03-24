2bedb1556f70a763c65e549d0e7e3a88e7077a14
NacreousDawn596 <kamalafriadwac@gmail.com>
2024-03-24 00:57:39 +0000

    idk tbh

--- ios/ClickClockTests/ClockTests.m
+++ ios/ClickClockTests/ClockTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ClockTests : XCTestCase
+@interface ClickClockTests : XCTestCase
 
 @end
 
-@implementation ClockTests
+@implementation ClickClockTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
